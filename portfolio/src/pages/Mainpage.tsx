012aa2b5e7a6214df6d4eef1bde4cc9119c27a32
Sandro <sandro.sembeni@gmail.com>
2022-07-26 21:30:08 +0200

    Add base project

---
+++ portfolio/src/pages/Mainpage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function Mainpage() {
+  return (
+    <div className="Mainpage">
+      <header className="Mainpage-header">
+        <img src={logo} className="Mainpage-logo" alt="logo" />
+        <p>
+          Edit <code>src/Mainpage.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Mainpage;
