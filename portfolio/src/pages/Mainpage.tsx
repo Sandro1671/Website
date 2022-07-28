9605d3cd0cf7ff975c3ac7a41a044a95a7cdebc0
Sandro <sandro.sembeni@gmail.com>
2022-07-28 23:48:16 +0200

    Add Navbar

--- portfolio/src/pages/Mainpage.tsx
+++ portfolio/src/pages/Mainpage.tsx
@@ -1,23 +1,7 @@
-import logo from '../logo.svg';
+import Navbar from '../components/Navbar';
 
 function Mainpage() {
-  return (
-    <div className="Mainpage">
-      <header className="Mainpage-header">
-        <img src={logo} className="Mainpage-logo" alt="logo" />
-        <p>
-          Edit <code>src/Mainpage.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Navbar></Navbar>;
 }
 
 export default Mainpage;
